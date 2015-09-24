a9b403e3d42d3a86961626bd13c4c603e2a1f2c5
Andrew Gilbert <andrewg800@gmail.com>
2015-09-24 14:28:28 -0700

    Add parseLines to csvhandler
    Also add test for reading from real file

--- lab1/csvhandler/csvhandler.py
+++ lab1/csvhandler/csvhandler.py
@@ -6,3 +6,6 @@ class CSVHandler:
     items = map(string.strip, items)
     items = map(lambda str: None if str == '' else str, items)
     return map(lambda str: None if str is None else float(str), items)
+
+  def parseLines(self, lines):
+    return map(lambda line: self.parseVector(line), lines)
--- lab1/csvhandler/test_csvhandler.py
+++ lab1/csvhandler/test_csvhandler.py
@@ -13,3 +13,23 @@ class TestReadVectors(unittest.TestCase):
 
     vec = handler.parseVector("1,")
     self.assertEqual(vec, [1,None])
+
+  def test_parseLines(self):
+    data = "1,2,3,4\n5,6,7".split("\n")
+    handler = csvhandler.CSVHandler()
+    vecList = handler.parseLines(data)
+    self.assertEqual(vecList, [[1,2,3,4],[5,6,7]])
+
+  def test_parseFile(self):
+    fh = open("test.csv", "r")
+    lines = fh.readlines()
+    handler = csvhandler.CSVHandler()
+    vecList = handler.parseLines(lines)
+    self.assertEqual(vecList, [[1,2,3,4],
+                               [4,5,6,7],
+                               [1.1,2.2,3.3,4.4],
+                               [1,2,3,4,5,6,7,7,8,9],
+                               [1],
+                               [None,None,None,None,None,4],
+                               [3,None,None,None,None],
+                               [None,None,None,None,None,None]])
