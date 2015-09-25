a82078b84db28fcd8e12ec0c2159e6a84b4f19e8
Andrew Gilbert <andrewg800@gmail.com>
2015-09-25 13:13:09 -0700

    Add more edge cases

--- lab1/texthandler/test_texthandler.py
+++ lab1/texthandler/test_texthandler.py
@@ -8,14 +8,15 @@ class TestTextHandler(unittest.TestCase):
         handler = TextHandler(fh)
         reader = handler.wordReader()
         words = list(reader)
-        self.assertEqual(['It', 'is', 'a', 'truth', 'universally', 'acknowledged'], words[0:6])
+        self.assertEqual(["It's", 'a', 'truth', 'universally', 'acknowledged'], words[0:4])
+        self.assertEqual('grown-up', words[894])
 
     def test_readSentences(self):
         fh = self.getFileHandle()
         handler = TextHandler(fh)
         reader = handler.sentenceReader()
         sentences = list(reader)
-        data = ["""It is a truth universally acknowledged, that a single man in possession
+        data = ["""It's a truth' 'universally acknowledged, that a single man in possession
 of a good fortune, must be in want of a wife.""",
 
 """However little known the feelings or views of such a man may be on his
@@ -23,9 +24,7 @@ first entering a neighbourhood, this truth is so well fixed in the minds
 of the surrounding families, that he is considered the rightful property
 of some one or other of their daughters.""",
 
-"""\
-"My dear Mr. Bennet," said his lady to him one day, "have you heard that
-Netherfield Park is let at last?"\
-"""]
+""""My dear Mr. Bennet," said his lady to him one day, "have you heard that
+Netherfield Park is let at last?\""""]
 
         self.assertEqual(data, sentences[0:2])
