f2b104ba046d277fc1afe421444eea755f43a31e
Andrew Gilbert <andrewg800@gmail.com>
2015-09-26 16:14:16 -0700

    Implement dot

--- lab1/vector/vector.py
+++ lab1/vector/vector.py
@@ -3,3 +3,6 @@ import math
 class Vector(list):
     def length(self):
         return math.sqrt(sum(i**2 for i in self))
+
+    def dot(self, vec):
+        return sum([i + j for i in self for j in vec])
