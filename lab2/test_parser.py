e1f897f2fc4484bad7ae5f17b1db2811be379436
Andrew Gilbert <andrewg800@gmail.com>
2015-09-30 14:00:00 -0700

    Update to interator version of processing chain

--- lab2/test_parser.py
+++ lab2/test_parser.py
@@ -2,21 +2,25 @@ import unittest
 from modelbuilder import ModelBuilder
 class TestModelBuilder(unittest.TestCase):
     def basicRead(self):
-        data = '[{"pid":100, "first":"Hannah-Beth", "last":"Jackson", "PersonType":"Legislator", "date":"28-04-15", "house":"Senate", "Committee":"Judiciary", "text":"Excuse me. Excuse me. You\'re opposed to the bill. Thank you very much. Please, name, affiliation, and position."}]'
+        fake = FakeElement(['thing', 'another'])
+        ret = next(fake)
+        utterances = fake.document
+        self.assertEqual('thing', ret)
+        self.assertEqual('thing', utterances)
 
-        builder = ModelBuilder()
-        fake = FakeElement()
-        builder.addModule(fake)
-        builder.read(StringIO(data))
-        utterances = fake.getDocument()
-        expected = {"pid" : 100, "first":"Hannah-Beth", "last":"Jackson", "PersonType":"Legislator", "date":"28-04-15", "house":"Senate", "Committee":"Judiciary", "text":"Excuse me. Excuse me. You're opposed to the bill. Thank you very much. Please, name, affiliation, and position."}
-        self.assertEqual(data, utterances)
+        ret = next(fake)
+        utterances = fake.document
+        self.assertEqual('another', ret)
+        self.assertEqual('another', utterances)
 
 
 class FakeElement:
-    def process(self, document):
-        self.document = document
-        return None
+    def __init__(self, parent):
+        self.parent = parent
 
-    def getDocument(self):
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.document = next(parent)
         return self.document
