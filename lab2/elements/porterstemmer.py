08aae8ca08fbd37317a659e2841fd49b2d6bc0cb
Andrew Gilbert <andrewg800@gmail.com>
2015-10-01 14:50:13 -0700

    Write stemmer

---
+++ lab2/elements/porterstemmer.py
@@ -0,0 +1,14 @@
+from nltk.stem.porter import PorterStemmer
+class PorterStemmerElement:
+    def __init__(self, parent):
+        self.__porter = PorterStemmer()
+        self.__parent = parent
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        document = next(self.__parent)
+        document['words'] = [self.__porter.stem(word) for word in document['words']]
+        return document
+
--- lab2/test_parser.py
+++ lab2/test_parser.py
@@ -1,6 +1,7 @@
 import unittest
 from elements.stopword import StopwordElement
 from document import Document
+from elements.porterstemmer import PorterStemmerElement
 class TestModelBuilder(unittest.TestCase):
     def testStopwords(self):
         doc_itr = FakeParser({'words': ['this', 'that', 'these', 'those']})
@@ -8,6 +9,12 @@ class TestModelBuilder(unittest.TestCase):
         self.assertEqual(next(sw_itr), ['this', 'that'])
 
     def testStemming(self):
+        doc_itr = FakeParser({'words': ['caresses', 'flies', 'dies', 'mules', 'denied']})
+        stem_itr = PorterStemmerElement(doc_itr)
+        self.assertEqual(next(stem_itr), document(['caress', 'fli', 'die', 'mule', 'deni']))
+
+def document(words):
+    return {'words': words}
 
 class FakeParser:
     def __init__(self, doc):
