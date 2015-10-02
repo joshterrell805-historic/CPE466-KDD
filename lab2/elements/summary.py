76034e40aa303e7c8ebc0488cbc3459c73d9d23f
Andrew Gilbert <andrewg800@gmail.com>
2015-10-02 15:59:22 -0700

    Implement document summary element

---
+++ lab2/elements/summary.py
@@ -0,0 +1,32 @@
+import math
+class SummaryElement:
+    def __init__(self, parent):
+        self.__parent = parent
+        self.__df = {}
+        self.__idf = None
+        self.N = 0
+
+    def DF(self):
+        return self.__df.copy()
+
+    def IDF(self):
+        if self.__idf:
+            return self.__idf.copy()
+        else:
+            self.__idf = {word: math.log2(self.N/dfi) for word, dfi in self.__df.items()}
+            return self.__idf
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        document = next(self.__parent)
+        self.N += 1
+        for word in document['words'].keys():
+            if word in self.__df:
+                self.__df[word] += 1
+            else:
+                self.__df[word] = 1
+        return document
+
+
--- lab2/test_parser.py
+++ lab2/test_parser.py
@@ -1,7 +1,8 @@
 import unittest
-from elements.stopword import StopwordElement
 from document import Document
+from elements.stopword import StopwordElement
 from elements.porterstemmer import PorterStemmerElement
+from elements.summary import SummaryElement
 class TestModelBuilder(unittest.TestCase):
     def testStopwords(self):
         doc_itr = FakeParser({'words': {'this': 3, 'that': 2, 'these': 4, 'those': 5}})
@@ -12,6 +13,16 @@ class TestModelBuilder(unittest.TestCase):
         doc_itr = FakeParser({'words': {'caresses': 3, 'flies': 4, 'dies': 5, 'mules': 6, 'mule': 4, 'denied': 7}})
         stem_itr = PorterStemmerElement(doc_itr)
         self.assertEqual(next(stem_itr), document({'caress': 3, 'fli': 4, 'die': 5, 'mule': 10, 'deni': 7}))
+
+    def testAccumulator(self):
+        documents = [document({'this': 3, 'that': 2, 'these': 4, 'those': 5}),
+                     document({'this': 2, 'those': 2})]
+        # Run all the documents through the summary filter
+        summary = SummaryElement(x for x in documents)
+        for doc in summary:
+            pass
+        self.assertEqual({'this': 2, 'that': 1, 'these': 1, 'those': 2}, summary.DF())
+        self.assertEqual({'this': 0, 'that': 1, 'these': 1, 'those': 0}, summary.IDF())
 
 def document(words):
     return {'words': words}
