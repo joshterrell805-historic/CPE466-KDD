d623a006cc2b8a27c6e55c8266e9f19f763417ba
Andrew Gilbert <andrewg800@gmail.com>
2015-10-03 11:51:20 -0700

    First basic index script

--- lab2/scripts/index.py
+++ lab2/scripts/index.py
@@ -1,10 +1,17 @@
 import click
-from jsonreader import JsonReader
+from elements.jsonreader import JsonReader
+from elements.freqcounter import FreqCounter
+from elements.stopword import StopwordElement
+from elements.porterstemmer import PorterStemmerElement
 
 @click.command()
 @click.argument('input', type=click.File('r'))
 def cli(input):
     """Example basic script"""
-    click.echo('Hello World!')
-    click.secho(input.read()[0:100], fg='green')
-    click.
+    doc_itr = JsonReader(input)
+    with click.progressbar(doc_itr) as prog_doc_itr:
+        freq_itr = FreqCounter(doc_itr, 'text')
+        stop_itr = StopwordElement(freq_itr, ['a', 'an', 'the'])
+        stem_itr = PorterStemmerElement(stop_itr)
+        res = list(stem_itr)
+    click.secho(str(res[0]), fg='green')
