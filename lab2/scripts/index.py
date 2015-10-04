42b0d675341f875840f42ee010a16cbce73ce8c2
Andrew Gilbert <andrewg800@gmail.com>
2015-10-03 18:59:19 -0700

    Add output to pickle to index tool
    Also hook up progress bar to work correctly

--- lab2/scripts/index.py
+++ lab2/scripts/index.py
@@ -3,15 +3,22 @@ from elements.jsonreader import JsonReader
 from elements.freqcounter import FreqCounter
 from elements.stopword import StopwordElement
 from elements.porterstemmer import PorterStemmerElement
+import pickle
+import sys
 
 @click.command()
 @click.argument('input', type=click.File('r'))
-def cli(input):
+@click.option('--output', type=click.File('wb'))
+def cli(input, output):
     """Example basic script"""
     doc_itr = JsonReader(input)
     with click.progressbar(doc_itr) as prog_doc_itr:
-        freq_itr = FreqCounter(doc_itr, 'text')
+        freq_itr = FreqCounter(prog_doc_itr, 'text')
         stop_itr = StopwordElement(freq_itr, ['a', 'an', 'the'])
         stem_itr = PorterStemmerElement(stop_itr)
         res = list(stem_itr)
-    click.secho(str(res[0]), fg='green')
+        if output:
+            pickle.dump(res, output)
+        else:
+            for doc in res:
+                click.echo(str(doc) + "\n")
