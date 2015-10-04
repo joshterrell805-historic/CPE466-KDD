2124b957a78953d5c74142dee56c35aa71115299
Josh Terrell <joshterrell805@gmail.com>
2015-10-03 22:25:07 -0700

    scripts/index save parsed docs to data files

--- lab2/scripts/index.py
+++ lab2/scripts/index.py
@@ -3,24 +3,30 @@ from elements.jsonreader import JsonReader
 from elements.freqcounter import FreqCounter
 from elements.stopword import StopwordElement
 from elements.lowercaser import LowerCaser
+from elements.summary import SummaryElement
 from elements.porterstemmer import PorterStemmerElement
 import pickle
 import sys
 
 @click.command()
 @click.argument('input', type=click.File('r'))
-@click.option('--output', type=click.File('wb'))
-def cli(input, output):
-    """Example basic script"""
+@click.option('--docspath', type=click.File('wb'),
+        default='data/parsed-docs.pkl')
+@click.option('--metapath', type=click.File('wb'),
+        default='data/doc-meta.pkl')
+def cli(input, docspath, metapath):
+    """Parse documents for word vectors and save."""
     doc_itr = JsonReader(input)
     with click.progressbar(doc_itr) as prog_doc_itr:
         freq_itr = FreqCounter(prog_doc_itr, 'text')
         lowr_itr = LowerCaser(freq_itr)
         stop_itr = StopwordElement(lowr_itr, ['a', 'an', 'the'])
         stem_itr = PorterStemmerElement(stop_itr)
-        res = list(stem_itr)
-        if output:
-            pickle.dump(res, output)
-        else:
-            for doc in res:
-                click.echo(str(doc) + "\n")
+        sumr_itr = SummaryElement(stem_itr)
+        docs = list(sumr_itr)
+        meta = {
+            'docFreq': sumr_itr.DF(),
+            'docCount': sumr_itr.N,
+        }
+        pickle.dump(docs, docspath)
+        pickle.dump(meta, metapath)
