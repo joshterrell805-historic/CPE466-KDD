a20f21cc09ebc7f20c03e2e244396ddd734f965a
Josh Terrell <joshterrell805@gmail.com>
2015-10-04 19:55:30 -0700

    don't error out on missing words in query

--- lab2/matching/cosinesimilarity.py
+++ lab2/matching/cosinesimilarity.py
@@ -4,17 +4,18 @@ from vector.vector import Vector
 class CosineSimilarity(Algorithm):
     def match(self, queryWords, docWords):
         words = self.sharedWords(queryWords, docWords)
-        invDocFreqs = [self.invDocFreq(w) for w in words]
+        idf = [self.invDocFreq(w) for w in words]
 
         docVec = self.wordVector(words, docWords)
         maxFreq = docVec.largest()
-        docVec = Vector([docVec[i] / maxFreq * invDocFreqs[i]
+        docVec = Vector([docVec[i] / maxFreq * idf[i]
                 for i in range(len(words))])
 
         queryVec = self.wordVector(words, queryWords)
         maxFreq = queryVec.largest()
-        queryVec = Vector([(0.5 + 0.5*queryVec[i]/maxFreq)*invDocFreqs[i]
-                for i in range(len(words))])
+        queryVec = Vector([0 if idf[i] == 0 else \
+                (0.5 + (0 if maxFreq == 0 else 0.5*queryVec[i]/maxFreq)) \
+                * idf[i] for i in range(len(words))])
 
         return queryVec.dot(docVec) / \
                 (queryVec.length() * docVec.length())
