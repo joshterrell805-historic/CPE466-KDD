80b0427881949bc81ba574646e5d34bf13729a8e
Andrew Gilbert <andrewg800@gmail.com>
2015-10-15 08:43:23 -0700

    Embed compile in setup script
    Also use c11 mode to keep my compiler happy (c99 would also have worked)

--- lab3/setup.py
+++ lab3/setup.py
@@ -3,6 +3,12 @@ setup(
     name = "PageRank-er",
     version = "0.1",
     install_requires=[
+        'cffi',
+    ],
+    setup_requires=[
         'cffi'
+    ],
+    cffi_modules=[
+        "build_page_rank.py:ffi"
     ],
 )
