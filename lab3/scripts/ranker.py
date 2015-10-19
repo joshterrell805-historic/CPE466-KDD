968a1825feb9c5288157534e3296b31f0384dba9
Josh Terrell <joshterrell805@gmail.com>
2015-10-19 14:02:56 -0700

    do a little cleanup

--- lab3/scripts/ranker.py
+++ lab3/scripts/ranker.py
@@ -13,53 +13,47 @@ def newGraph(maxNodes, epsilon, dVal, threads):
 
     return lib.newGraph(maxNodes, batchSize, dVal, epsilon, threads)
 
+def addEdge(graph, line):
+    parts = line.split(',')
+
+    left = parts[0].strip().strip('"')
+    right = parts[2].strip().strip('"')
+
+    lib.addEdge(
+            graph, left.encode(encoding="ascii"),
+            right.encode(encoding="ascii"))
+
+    return [left, right]
+
 @click.command()
 @click.option('--epsilon', help='If every node changes less than epsilon in an iteration, consider pagerank of the network to have converged.', default=0.00001)
 @click.option('--maxiterations', default=100)
 @click.option('--dval', help='Probability of following a link in pagerank algorithm.', default=0.85)
 @click.option('--threads', help='number of threads computing pagerank.', default=4)
+@click.option('--undirected', help='Specify flag to indicate that this source file contains undirected node data.', is_flag=True)
 @click.argument('datafile', type=click.File('r'))
-def rank(epsilon, maxiterations, dval, threads, datafile):
-    nameToIndex = {}
-    lines = []
+def rank(epsilon, maxiterations, dval, threads, datafile, undirected):
+
     start = time.clock()
-    size = 0
+
+    maxNodes = 0
     for line in datafile:
-        size += 1
+        maxNodes += 1
+
+    maxNodes = maxNodes * 2 if undirected else maxNodes
+    graph = newGraph(maxNodes, epsilon, dval, threads)
     datafile.seek(0)
-    for line in datafile:
-        lines.append(line)
-    setuptime = time.clock() - start
-    print(setuptime)
-
-    graph = newGraph(len(lines), epsilon, dval, threads)
-
     nodes = set()
-#    import pdb; pdb.set_trace()
-    print("Adding lines")
-    num = 0
-    for edge in lines:
-        parts = edge.split(',')
 
-        left = parts[0].strip().strip('"')
-        right = parts[2].strip().strip('"')
+    for line in datafile:
+        nodes.update(addEdge(graph, line))
 
-        nodes.add(left)
-        nodes.add(right)
-        num += 1
-        print("{0}->{1}^{2}".format(num, left, right))
-        lib.addEdge(
-                graph, left.encode(encoding="ascii"),
-                right.encode(encoding="ascii"))
-        
-    print("Added lines")
+    loadtime = time.clock() - start
+    print("Load Time:", loadtime)
 
+    # todo undirected stuff
     for i in range(maxiterations):
         lib.computeIteration(graph)
-        # thread = threading.Thread(target= args=(isSourceA))
-        # thread.run()
-        #print("Iterating...")
-        # thread.join()
         if graph.converged == 1:
             break
 
