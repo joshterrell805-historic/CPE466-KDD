261b145faadebbdcb68a8ebb936983db743b01a8
Josh Terrell <joshterrell805@gmail.com>
2015-11-03 14:46:46 -0800

    add Node and Label classes

---
+++ lab4/scripts/model.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, name, *edge_tuples):
+        self.name = name
+        self.edges = {}
+        for edge in edge_tuples:
+            name = 0
+            label = 1
+            self.edges[edge[name]] = edge[label]
+
+class Label:
+    def __init__(self, category):
+        self.category = category
