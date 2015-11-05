ec3d3c81a27243393acb9df470b63be4d68712c4
Andrew Gilbert <andrewg800@gmail.com>
2015-11-05 06:29:10 -0800

    Implement hashtable and make tests pass

--- ParallelPageRank/src/hashtable.h
+++ ParallelPageRank/src/hashtable.h
@@ -5,5 +5,6 @@ typedef struct {
 } map;
 
 map *createMap(int size);
+void destroyMap(map *garbage);
 int getItem(map *hashtable, unsigned int key);
 void addItem(map *hashtable, unsigned int key, int value);
