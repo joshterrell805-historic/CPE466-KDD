bce0ebcdc1f7b4331b40e7ba53753fca64f1d400
Josh Terrell <joshterrell805@gmail.com>
2015-11-05 12:49:24 -0800

    lab4: build tree from string

--- lab4/scripts/model.py
+++ lab4/scripts/model.py
@@ -1,3 +1,5 @@
+import xml.etree.ElementTree as ET
+
 class Node:
     def __init__(self, name, *edge_tuples):
         self.name = name
@@ -13,3 +15,40 @@ class Label:
 
     def __eq__(self, other):
         return self.category == other.category
+
+def build_tree(xml_str):
+    xml_tree = ET.fromstring(xml_str)
+    xml_root = xml_tree #xml_tree.getroot()
+    children = list(xml_root)
+
+    if not (xml_root.tag == "Tree" and 'name' in xml_root.keys() and \
+            len(children) == 1 and children[0].tag == 'node'):
+        raise Exception('Invalid Tree root node')
+
+    return build_tree_recursive(children[0])
+
+def build_tree_recursive(xml_root):
+    if xml_root.tag == 'node':
+        if 'var' not in xml_root.keys():
+            raise Exception('nodes must have "var" attribute')
+
+        xml_children = list(xml_root)
+        if len(xml_children) < 2 or\
+                False in [c.tag == 'edge' for c in xml_children]:
+            raise Exception('nodes must have >= 2 edge children')
+        return Node(xml_root.get('var'),\
+                *[build_tree_recursive(c) for c in xml_children])
+    elif xml_root.tag == 'edge':
+        if 'var' not in xml_root.keys() or 'num' not in xml_root.keys():
+            raise Exception('edges must have "var" and "num" attributes')
+
+        xml_children = list(xml_root)
+        if len(xml_children) != 1 or xml_children[0].tag == 'edge':
+            raise Exception('edges must have exactly 1 non-edge child')
+        return (xml_root.get('var'), build_tree_recursive(xml_children[0]))
+    elif xml_root.tag == 'decision':
+        if 'choice' not in xml_root.keys() or 'end' not in xml_root.keys():
+            raise Exception('leafs must have "choice" and "end" attributes')
+        return Label(xml_root.get('choice'))
+    else:
+        raise Exception('tree elements must be "node" or "edge"')
--- lab4/scripts/test_model.py
+++ lab4/scripts/test_model.py
@@ -1,5 +1,6 @@
 import unittest
 from model import Node, Label
+import model
 
 class TestModel(unittest.TestCase):
     def test_build_model(self):
@@ -24,3 +25,53 @@ class TestModel(unittest.TestCase):
         self.assertEqual(node.name, "Has chicken in burritos")
         self.assertEqual(node.edges['true'], Label("5 stars"))
         self.assertEqual(node.edges['false'], Label("3 stars"))
+
+    def test_build_tree(self):
+        tree_str = """
+        <Tree name = "test">
+          <node var ="Gender">
+            <edge var ="Female" num="2">
+              <node var = "Bush Approval">
+                <edge var = "Approve" num="2" >
+                  <decision end = "2" choice = "McCain" p = "0.9"/>
+                </edge>
+                <edge var = "Disapprove" num="1">
+                  <decision end = "1" choice="Obama" p = "0.95"/>
+                </edge>
+              </node>
+            </edge>
+            <edge var = "Male" num="1">
+              <node var = "Ideology">
+                <edge var = "Liberal" num = "1">
+                  <decision end = "1" choice ="Obama" p = "0.99"/>
+                </edge>
+                <edge var = "Moderate" num="2">
+                  <decision end = "1" choice = "Obama" p = "0.7"/>
+                </edge>
+                <edge var = "Conservative" num ="3">
+                  <decision end = "2" choice = "McCain" p = "0.95"/>
+                </edge>
+              </node>
+            </edge>
+          </node>
+        </Tree>
+        """
+
+        root = model.build_tree(tree_str)
+        self.assertEqual(root.name, "Gender")
+        self.assertEqual(len(root.edges.keys()), 2)
+
+        # Gender = Female
+        node = root.edges['Female']
+        self.assertEqual(node.name, "Bush Approval")
+        self.assertEqual(len(node.edges.keys()), 2)
+        self.assertEqual(node.edges['Approve'], Label("McCain"))
+        self.assertEqual(node.edges['Disapprove'], Label("Obama"))
+
+        # Gender = Male
+        node = root.edges['Male']
+        self.assertEqual(node.name, "Ideology")
+        self.assertEqual(len(node.edges.keys()), 3)
+        self.assertEqual(node.edges['Liberal'], Label("Obama"))
+        self.assertEqual(node.edges['Moderate'], Label("Obama"))
+        self.assertEqual(node.edges['Conservative'], Label("McCain"))
