6897c64f8831facedba4d97373ad3f8410d74f44
Josh Terrell <joshterrell805@gmail.com>
2015-11-05 14:09:00 -0800

    organize model script

--- lab4/model.py
+++ lab4/model.py
@@ -30,6 +30,14 @@ def build_tree(xml_str):
 
     return build_tree_recursive(children[0])
 
+def stringify_tree(tree):
+    node = build_xml_tree_recursive(tree)
+    root = ET.Element('Tree', {'name': '?'})
+    root.append(node)
+    return ET.tostring(root)
+
+###### helper functions #######
+
 def build_tree_recursive(xml_root):
     if xml_root.tag == 'node':
         if 'var' not in xml_root.keys():
@@ -55,12 +63,6 @@ def build_tree_recursive(xml_root):
         return Label(xml_root.get('choice'))
     else:
         raise Exception('tree elements must be "node" or "edge"')
-
-def stringify_tree(tree):
-    node = build_xml_tree_recursive(tree)
-    root = ET.Element('Tree', {'name': '?'})
-    root.append(node)
-    return ET.tostring(root)
 
 def build_xml_tree_recursive(tree):
     if type(tree) == Node:
