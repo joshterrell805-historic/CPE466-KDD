271ff9c62e7c2c5366a8b08f39cf67344ea44748
Josh Terrell <joshterrell805@gmail.com>
2015-11-05 19:52:03 -0800

    lab4: implement requirement: plurality of -1 (id) is exlcuded

--- lab4/dataset.py
+++ lab4/dataset.py
@@ -1,23 +1,26 @@
 def read(raw, restrictions=None):
-    row_strs = list(raw.splitlines())[4:]
+    row_strs = list(raw.strip().splitlines())
     cols = [set() for _ in range(row_strs[0].count(',') + 1)]
-    rows = []
 
-    if restrictions and len(restrictions) + 1 != len(cols):
+    if restrictions == None:
+        restrictions = [True] * (len(cols) - 1)
+    elif len(restrictions) + 1 != len(cols):
         raise Exception('len(restrictions) != #features')
 
-    for row in row_strs:
+    pluralities = [int(x) for x in row_strs[1].split(',')]
+    for i,p in enumerate(pluralities):
+        if p == -1:
+            restrictions[i] = False
+
+    rows = []
+    for row in row_strs[3:]:
         row = row.split(',')
         for k,v in enumerate(row):
             cols[k].add(v)
-        if restrictions:
-            attrs = [c for i,c in enumerate(row[:-1]) if restrictions[i]]
-        else:
-            attrs = row[:-1]
+        attrs = [c for i,c in enumerate(row[:-1]) if restrictions[i]]
         rows.append((attrs, row[-1]))
 
-    if restrictions:
-        cols = [c for i,c in enumerate(cols) if i + 1 == len(cols) \
-                or restrictions[i]]
+    cols = [c for i,c in enumerate(cols) if i + 1 == len(cols) \
+            or restrictions[i]]
 
     return cols, rows
