b64128a57b0126ec54e64a92832f8a50d8dd73ca
Josh Terrell <joshterrell805@gmail.com>
2015-11-05 18:48:31 -0800

    lab4: structure of induce/classify script

--- lab4/scripts/induce_c45.py
+++ lab4/scripts/induce_c45.py
@@ -1,6 +1,17 @@
 # Train a decision tree on the input data and print it to stdout.
-import click
+import click, dataset
+from model import Label, Node, stringify_tree
 
 @click.command()
-def main():
-    pass
+@click.argument('DomainXML')
+@click.argument('TrainingSetCSV', type=click.File('r'))
+@click.argument('RestrictionsTXT', required=False, type=click.File('r'))
+def main(domainxml, trainingsetcsv, restrictionstxt):
+    col_sets, data = dataset.read(trainingsetcsv.read())
+    # call train function with:
+    #   `col_sets` - list of sets per column, including class label
+    #   `data` (list of ([train data], class))
+    tree = Node("swole", ("true", Label("protein and starches")),
+            ("false", Label("sugar"))) # dummy temp tree
+    tree_xml = stringify_tree(tree)
+    print(tree_xml)
