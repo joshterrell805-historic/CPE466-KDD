80db962a7de5e110d40574424b045518138b698d
moyodancer <yost@calpoly.edu>
2015-11-09 19:04:56 -0800

    added main to try it out and debgged compile errors

--- ParallelPageRank/src/getRank.c
+++ ParallelPageRank/src/getRank.c
@@ -1,15 +1,33 @@
 #include "mkl.h"
-   
-void makeP(float *Avals, MKL_INT *rowind, MKL_INT *numRow, MKL_INT *colind, MKL_INT *nnz, int n, float dP){
+#include <stdio.h>
+void makeP(float *Avals, MKL_INT *rowind, MKL_INT *numRow, MKL_INT *colind, MKL_INT *nnz, int n, float dP);
+void getRank(float *Pvals, MKL_INT *rowind, MKL_INT *colind, MKL_INT *numRows, MKL_INT numCols, MKL_INT *nnz, float tol, int N, float dP);
+float sum(float *x, int N);
+void ones(float *a, int N);
 
-   float *d = (float*)malloc(sizeof(float)*N);
+int main(int argc, char *argv[]){
+   MKL_INT nnz = 14;
+   int N = 36;
+   float Avals[14];
+   ones((float*)Avals, (int)nnz);
+   MKL_INT rowind[14] = {0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4};
+   MKL_INT colind[14] = {1, 2, 3, 4, 1, 3, 4, 1, 2, 4, 1, 2, 3, 5};
+   MKL_INT numRow = 6;
+   float dP = .95;
+   makeP(Avals, rowind, &numRow, colind, &nnz, N, dP);
+   return 0;
+}
+
+void makeP(float *Avals, MKL_INT *rowind, MKL_INT *numRow, MKL_INT *colind, MKL_INT *nnz, int N, float dP){
+
+   float *d = (float*)malloc(sizeof(float)*(*numRow));
    int i;
-   for(i = 0; i<N; i++){
+   for(i = 0; i<*numRow; i++){
       d[i] = 1;
    }
    char transa = 'N';
-   mkl_cspblas_scoogemv (&tansa, numRow, Avals ,rowind , colind , nnz , d, d );
-   for(i = 0; i<N; i++){
+   mkl_cspblas_scoogemv (&transa, numRow, Avals ,rowind , colind , nnz , d, d );
+   for(i = 0; i<*numRow; i++){
      printf("d[%d] = %lf\n", i, d[i]); 
    }
    for (i = 0; i<*nnz; i++){
@@ -17,28 +35,27 @@ void makeP(float *Avals, MKL_INT *rowind, MKL_INT *numRow, MKL_INT *colind, MKL_
    }
 } 
 
-void getRank(float *Pvals, MKL_INT *rowind, MKL_INT *colind, MKL_INT *numRows, MKL_INT numCols, MKL_INT *nnz, float tol){
+void getRank(float *Pvals, MKL_INT *rowind, MKL_INT *colind, MKL_INT *numRows, MKL_INT numCols, MKL_INT *nnz, float tol, int N, float dP){
 
-   float *z = (float*)malloc(sizeof(float)*N);
+   float *y = (float*)malloc(sizeof(float)*N);
+   ones(y, N);
    float *x = (float*)malloc(sizeof(float)*N);
    int i;
    for(i = 0; i<N; i++){
-      z[i] = 1;
       x[i] = 1/N;
    }
-   float *y = z;
    char transa = 'N';
    float alpha = 1;
    float beta = ((1-dP)/N)*sum(x, N);
-   char[6] matdescra = {'G', 'L', 'N','C'};
+   char matdescra[6] = {'G', 'L', 'N','C'};
    float error = 10;
    while(N*error/dP >tol){
       //call mat mult
       error = x[1];
-      mkl_scoomv (&transa, numRows, numRows, &alpha ,matdescra , Pvals , 
+      mkl_scoomv(&transa, numRows, numRows, &alpha ,matdescra , Pvals ,
                   rowind , colind , nnz , x , &beta , y );
       x = y;
-      y = z;
+      ones(y, N);
       beta = ((1-dP)/N)*sum(x, N);
       error -=x[1];
    }
@@ -51,4 +68,11 @@ float sum(float *x, int N){
       result+= x[i];
    }
    return result;
+}
+
+void ones(float *a, int N){
+   int i;
+   for (i =0; i< N; i++) {
+      a[i] = 1;
+   }
 }
