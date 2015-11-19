b6ac2936144e9979f242c9492886caf4077028f5
Andrew Gilbert <andrewg800@gmail.com>
2015-11-19 06:33:13 -0800

    Finish implementation of helpers and clean up

--- lab4/c45.py
+++ lab4/c45.py
@@ -1,26 +1,103 @@
 from model import Node, Label
 import math
+import itertools
 
 def run(d, attributes, threshold):
-    return Label("done")
+    maybeLeaf = has_single_class(d)
+    if maybeLeaf:
+        return maybeLeaf
+    else:
+        maybeLeaf = has_no_attrib(d)
+        if maybeLeaf:
+            return maybeLeaf
+        else:
+            Ag = select_splitting_attribute_idx(d, len(attributes), threshold)
+            if not Ag:
+                return Label(find_most_frequent_label(d))
+            else:
+                dom = set(t[0][Ag] for t in d)
+                nodes = []
+                for v in dom:
+                    Dv = [t for t in d if t[Ag] == v]
+                    if len(Dv) > 0:
+                        nodes.append((Ag, run(Dv, set(attributes) - set([Ag]), threshold)))
+                return Node(Ag, *nodes)
 
-def select_splitting_attribute(d, threshold):
+def select_splitting_attribute_idx(d, attr_count, threshold):
     p0 = entropy(d)
+    p = [entropy_wrt(d, i) for i in range(attr_count)]
+    gain = [p0 - p[i] for i in range(attr_count)]
+    idx = idx_of_max(gain)
+    if idx and gain[idx] > threshold:
+        return idx
+    else:
+        return
 
-def entropy_ci(category, d):
-    D_i = len([t for t in d if t[1] == category])
+def entropy_ci(cat, d):
+    D_i = sum(1 for t in d if category(t) == cat)
     D = len(d)
     Pr = D_i/D
     return Pr * math.log2(Pr)
 
 def entropy(d):
-    categories = set(t[1] for t in d)
+    categories = set(category(t) for t in d)
     return -sum(entropy_ci(c, d) for c in categories)
 
-def entropy_wrt(d, attr):
-    values = set(t[0][attr] for t in d)
-    def entropy_j(j):
-        Dj = [t for t in d if t[0][attr] == j]
-        return len(Dj)/len(d) * entropy(Dj)
-    return sum([entropy_j(j) for j in values])
+def entropy_wrt(d, attr_idx):
+    value_sets = []
+    def keyfunc(t):
+        return attrib(t)[attr_idx]
+    data = sorted(d, key=keyfunc)
+    for value, all_values in itertools.groupby(data, keyfunc):
+        value_sets.append(list(all_values))
+    return sum(len(Dj)/len(d) * entropy(Dj) for Dj in value_sets)
 
+def has_single_class(D):
+    cls = D[0][1]
+    for d in D:
+        if d[1] != cls:
+            return None
+    return Label(cls)
+
+def find_most_frequent_label(D):
+    classes = {}
+    for d in D:
+        cls = category(d)
+        if cls in classes:
+            classes[cls] += 1
+        else:
+            classes[cls] = 1
+    return idx_of_max(classes, classes.keys())
+
+def has_no_attrib(D, attributes):
+    if len(attributes) == 0:
+        maxKey = find_most_frequent_label(D)
+        return Label(maxKey)
+    else:
+        return None
+    
+def idx_of_max(lst, keys=None):
+    length = len(lst)
+    if length == 0:
+        return None
+
+    if not keys:
+        keys = range(length)
+
+    maxKey = None
+    maxVal = float('-inf')
+    for key in keys:
+        val = lst[key]
+        if val > maxVal:
+            maxVal = val
+            maxKey = key
+    return maxKey
+
+def first(lst):
+    return lst[0]
+
+def attrib(pair):
+    return pair[0]
+
+def category(pair):
+    return pair[1]
--- lab4/test_c45.py
+++ lab4/test_c45.py
@@ -3,7 +3,13 @@ import c45
 from model import Node, Label
 
 class TestC45(unittest.TestCase):
-    @unittest.skip("ded")
+    def setUp(self):
+        self.d = [([1, 2], 1),
+                  ([2, 2], 1),
+                  ([2, 1], 2)]
+        self.attr = ["color", "shape"]
+
+    @unittest.skip("Need attributes to behave")
     def test_basic(self):
         d = [([3,        "false",        "traditional",        "South"],        "Not Visited"),
              ([3,        "true",        "traditional",        "South"],        "Visited"),
@@ -36,19 +42,37 @@ class TestC45(unittest.TestCase):
                                        (4, Label("Visited")))))
         self.assertEqual(result, expected)
 
+    @unittest.skip("Need attributes to behave")
+    def test_small_run(self):
+        result = c45.run(self.d, [0,1], 0)
+        self.assertEqual(result, Node("shape",
+                                      (1, Label(2)),
+                                      (2, Label(1))))
+        print(result)
+
     def test_splitting_select(self):
-        d = [([1, 2], 1),
-             ([2, 2], 1),
-             (
-        c45.select_splitting_attribute(
+        splitting = c45.select_splitting_attribute_idx(self.d, len(self.attr), 0.1)
+        self.assertEqual(splitting, 1)
 
     def test_entropy(self):
-        d = [("this", 1),
-             ("that", 1),
-             ("fish", 3)]
-        self.assertAlmostEqual(c45.entropy(d), 0.9182958)
+        self.assertAlmostEqual(c45.entropy(self.d), 0.9182958)
+        
     def test_entropy_wrt(self):
-        d = [(["this"], 1),
-             (["this"], 1),
-             (["fish"], 3)]
-        self.assertAlmostEqual(c45.entropy_wrt(d, 0), 0)
+        self.assertAlmostEqual(c45.entropy_wrt(self.d, 1), 0)
+
+    def test_has_single_class(self):
+        self.assertEqual(c45.has_single_class(self.d[0:2]), Label(1))
+        self.assertFalse(c45.has_single_class(self.d))
+
+    def test_has_no_attrib(self):
+        self.assertEqual(c45.has_no_attrib([([], 2),
+                                            ([], 3),
+                                            ([], 2)], []), Label(2))
+        self.assertEqual(c45.has_no_attrib([([], 3),
+                                            ([], 2)], []), Label(2))
+        self.assertEqual(c45.has_no_attrib([([1], 3)], ["fish"]), None)
+    def test_find_most_frequent_label(self):
+        self.assertEqual(c45.find_most_frequent_label(self.d), 1)
+
+    def test_idx_of_max(self):
+        self.assertEqual(c45.idx_of_max([1,3,5,4]), 2)
