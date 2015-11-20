41f6f1c3c05923eae9b3ae645dd84b4922fc6674
Andrew Gilbert <andrewg800@gmail.com>
2015-11-19 21:43:13 -0800

    Work on validation script

--- lab4/scripts/validation.py
+++ lab4/scripts/validation.py
@@ -1,6 +1,17 @@
 # Perform cross-validation testing given a decision tree and dataset.
-import click
+import click, dataset
+import c45, sampling
+from model import Label, Node, stringify_tree
 
 @click.command()
-def main():
-    pass
+@click.argument('DomainXML')
+@click.argument('TrainingSetCSV', type=click.File('r'))
+@click.argument('manifold_value', type=click.INT)
+@click.argument('RestrictionsTXT', required=False, type=click.File('r'))
+def main(domainxml, trainingsetcsv, manifold_value, restrictionstxt):
+    restrictions = dataset.restrictions_from_text(restrictionstxt)
+
+    cols, data = dataset.read(trainingsetcsv.read(), restrictions)
+    expected, actual = sampling.cross_validate(data, list(cols), manifold_value)
+    print(expected)
+    print(actual)
