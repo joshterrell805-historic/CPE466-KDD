9390fd763af918b73099d10241580c588cd5f945
Andrew Gilbert <andrewg800@gmail.com>
2015-11-19 17:29:57 -0800

    Make read return the list of attribute headers
    Stop returning all values for each attribute; it's not the input c45 expects

--- lab4/dataset.py
+++ lab4/dataset.py
@@ -1,6 +1,6 @@
 def read(raw, restrictions=None):
     row_strs = list(raw.strip().splitlines())
-    cols = [set() for _ in range(row_strs[0].count(',') + 1)]
+    cols = row_strs[0].split(',')
 
     if restrictions == None:
         restrictions = [True] * (len(cols) - 1)
@@ -15,8 +15,6 @@ def read(raw, restrictions=None):
     rows = []
     for row in row_strs[3:]:
         row = row.split(',')
-        for k,v in enumerate(row):
-            cols[k].add(v)
         attrs = [c for i,c in enumerate(row[:-1]) if restrictions[i]]
         rows.append((attrs, row[-1]))
 
--- lab4/test_dataset.py
+++ lab4/test_dataset.py
@@ -11,14 +11,10 @@ Vote
 5,Democratic,Moderate,White,Male,Catholic,Less than 30000,College,30-44,Midwest,Disapprove,McCain
 """
     def test_read_dataset(self):
-        col_sets, data = dataset.read(self.dataset_str)
-        self.assertEqual(col_sets, [set(["Independent", "Republican",
-                "Democratic"]),set(["Moderate", "Liberal"]),set(["White"]),
-                set(["Male","Female"]),set(["Catholic","Protestant"]),set([
-                "30000-49999","50000-74999","Less than 30000"]),set(["College",
-                "H.S. diploma or less"]),set(["45-64","30-44"]),
-                set(["Northeast","Midwest"]),set(["Approve","Disapprove"]),
-                set(["McCain","Obama"])])
+        cols, data = dataset.read(self.dataset_str)
+        self.assertEqual(cols, ['Political Party', 'Ideology',
+        'Race', 'Gender', 'Religion', 'Family Income', 'Education',
+        'Age', 'Region', 'Bush Approval', 'Vote'])
         self.assertEqual(data,
                 [(["Independent","Moderate","White", "Male", "Catholic",
                     "30000-49999","College","45-64","Northeast",
@@ -31,10 +27,9 @@ Vote
                     "Disapprove"],"McCain")])
 
     def test_read_dataset_with_restrictions(self):
-        col_sets, data = dataset.read(self.dataset_str, [0,1,0,0,1,0,0,0,0,0,1])
-        self.assertEqual(col_sets, [set(["Independent", "Republican",
-                "Democratic"]),set(["Male","Female"]),
-                set(["Approve","Disapprove"]),set(["McCain","Obama"])])
+        cols, data = dataset.read(self.dataset_str, [0,1,0,0,1,0,0,0,0,0,1])
+        self.assertEqual(cols, ['Political Party', 'Gender',
+                                'Bush Approval', 'Vote'])
         self.assertEqual(data,
                 [(["Independent","Male","Approve"],"McCain"),
                  (["Republican","Female","Approve"],"Obama"),
