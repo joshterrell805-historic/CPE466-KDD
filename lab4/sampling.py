9f40c7e82bea623b9721c9e16553dd668593bb93
Andrew Gilbert <andrewg800@gmail.com>
2015-11-19 19:41:07 -0800

    Last three metric functions

--- lab4/sampling.py
+++ lab4/sampling.py
@@ -10,8 +10,21 @@ def recall(expected, actual, positive):
     FN = sum(1 for e, a in paired if e != a and a != positive)
     return TP / (TP + FN)
 
+def pf(expected, actual, positive):
+    paired = list(zip(expected, actual))
+    FP = sum(1 for e, a in paired if e != a and a == positive)
+    TN = sum(1 for e, a in paired if e == a and a != positive)
+    return FP / (FP + TN)
+
+def f_measure(expected, actual, positive):
+    prec = precision(expected, actual, positive)
+    rec = recall(expected, actual, positive)
+    return (2 * prec * rec) / (prec * rec)
+
 def accuracy(expected, actual):
     paired = list(zip(expected, actual))
     Dtrue = sum(1 for e, a in paired if e == a)
     return float(Dtrue) / float(len(expected))
 
+def error_rate(expected, actual):
+    return 1 - accuracy(expected, actual)
--- lab4/test_sampling.py
+++ lab4/test_sampling.py
@@ -33,6 +33,36 @@ class TestSampling(unittest.TestCase):
         FN = 1
         self.assertEqual(result, TP/(TP+FN))
 
+    def test_pf(self):
+        # The values in the training set
+        expected = ['a', 'b', 'a', 'b', 'a']
+        # The results from the classifier
+        actual = ['a', 'b', 'a', 'a', 'b']
+        # As though we're asking the question "does record belong in class a?"
+        positive = 'a'
+        result = sampling.pf(expected, actual, positive)
+        TP = 2
+        TN = 1
+        FP = 1
+        FN = 1
+        self.assertEqual(result, FP/(FP + TN))
+        
+    def test_f_measure(self):
+        # The values in the training set
+        expected = ['a', 'b', 'a', 'b', 'a']
+        # The results from the classifier
+        actual = ['a', 'b', 'a', 'a', 'b']
+        # As though we're asking the question "does record belong in class a?"
+        positive = 'a'
+        result = sampling.f_measure(expected, actual, positive)
+        TP = 2
+        TN = 1
+        FP = 1
+        FN = 1
+        precision = TP/(TP+FP)
+        recall = TP/(TP+FN)
+        self.assertEqual(result, 2 * precision * recall / (precision * recall))
+
     def test_accuracy(self):
         # The values in the training set
         expected = ['a', 'b', 'a', 'b', 'a']
@@ -41,3 +71,13 @@ class TestSampling(unittest.TestCase):
         # As though we're asking the question "does record belong in class a?"
         result = sampling.accuracy(expected, actual)
         self.assertEqual(result, 3/5)
+
+    def test_error_rate(self):
+        # The values in the training set
+        expected = ['a', 'b', 'a', 'b', 'a']
+        # The results from the classifier
+        actual = ['a', 'b', 'a', 'a', 'b']
+        # As though we're asking the question "does record belong in class a?"
+        result = sampling.error_rate(expected, actual)
+        self.assertEqual(result, 1 - 3/5)
+        
