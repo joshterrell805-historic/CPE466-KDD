4cfcd3d89c5195590dfcbc73649347452838d06e
Andrew Gilbert <andrewg800@gmail.com>
2015-11-19 17:50:11 -0800

    Running version of c45
    Doesn't get the results I expect; I'm not sure why.

--- lab4/scripts/induce_c45.py
+++ lab4/scripts/induce_c45.py
@@ -1,5 +1,6 @@
 # Train a decision tree on the input data and print it to stdout.
 import click, dataset
+import c45
 from model import Label, Node, stringify_tree
 
 @click.command()
@@ -13,11 +14,12 @@ def main(domainxml, trainingsetcsv, restrictionstxt):
         restrictions = [False if x == '0' else True \
                 for x in restrictionstxt.read().split(',')]
 
-    col_sets, data = dataset.read(trainingsetcsv.read(), restrictions)
+    cols, data = dataset.read(trainingsetcsv.read(), restrictions)
     # call train function with:
     #   `col_sets` - list of sets per column, including class label
     #   `data` (list of ([train data], class))
     tree = Node("swole", ("true", Label("protein and starches")),
             ("false", Label("sugar"))) # dummy temp tree
+    tree = c45.run(data, list(enumerate(cols)), 0)
     tree_xml = stringify_tree(tree)
     print(tree_xml)
