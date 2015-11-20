96e632a1a0020684d7cf07dd4032e768c7ca653d
Andrew Gilbert <andrewg800@gmail.com>
2015-11-19 22:28:56 -0800

    Add proper output for validator

--- lab4/scripts/validation.py
+++ lab4/scripts/validation.py
@@ -12,6 +12,30 @@ def main(domainxml, trainingsetcsv, manifold_value, restrictionstxt):
     restrictions = dataset.restrictions_from_text(restrictionstxt)
 
     cols, data = dataset.read(trainingsetcsv.read(), True, restrictions)
-    expected, actual = sampling.cross_validate(data, list(cols), manifold_value)
-    print(expected)
-    print(actual)
+    expected, actual, expected_hunked, actual_hunked = sampling.cross_validate(data, list(cols), manifold_value)
+    print("Overall confusion matrix:")
+    print(sampling.confusion_matrix(expected, actual))
+
+    print("\nOverall recall:")
+    print(sampling.recall(expected, actual, 'Obama'))
+
+    print("\nOverall precision:")
+    print(sampling.precision(expected, actual, 'Obama'))
+
+    print("\nOverall pf:")
+    print(sampling.pf(expected, actual, 'Obama'))
+
+    print("\nOverall f-measure:")
+    print(sampling.f_measure(expected, actual, 'Obama'))
+
+    print("\nOverall accuracy:")
+    print(sampling.accuracy(expected, actual))
+
+    print("\nAverage accuracy:")
+    print(sum(sampling.accuracy(e, a) for e, a in zip(expected_hunked, actual_hunked))/len(expected_hunked))
+
+    print("\nOverall error rate:")
+    print(sampling.error_rate(expected, actual))
+
+    print("\nAverage error rate:")
+    print(sum(sampling.error_rate(e, a) for e, a in zip(expected_hunked, actual_hunked))/len(expected_hunked))
