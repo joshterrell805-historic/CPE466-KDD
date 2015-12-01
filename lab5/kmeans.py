4f59dcc8271ffb525a240148b254930fec7a6e49
Andrew Gilbert <andrewg800@gmail.com>
2015-12-01 14:32:47 -0800

    Use numpy and pandas to read data

--- lab5/kmeans.py
+++ lab5/kmeans.py
@@ -1,11 +1,17 @@
-import reader
-import Clusterer from kmeans_clusterer
+from kmeans_clusterer import Clusterer
 import formatter
 from sklearn.pipeline import Pipeline
+import numpy as np
+import pandas as pd
 
+@click.command()
+@click.argument('datafile', type=click.File('r'))
+@click.argument('k', type=click.INT)
 def main(datafile, k):
-    data = reader.read_restricted(datafile)
+    header = datafile.readline()
+    collist = [i for (i, toggle) in zip(count(), header.split()) if toggle == "0"]
+    data = pd.read_csv(datafile, usecols = collist)
     pipeline = Pipeline(steps=[('cluster', Clusterer(kmeans=k))])
-    pipeline.fit()
-    clusters = pipeline.cluster()
+    pipeline.fit(data)
+    clusters = pipeline.cluster(data)
     print(formatter.format(clusters))
