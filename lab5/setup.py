a69cd1938e6940b1a82460b7f76513307035b94d
Andrew Gilbert <andrewg800@gmail.com>
2015-12-01 18:45:19 -0800

    Add setup.py

---
+++ lab5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+    name = "Grapevine",
+    version = "0.1",
+    install_requires=[
+        'click',
+        'numpy',
+        'pandas',
+        'scikit-learn'
+    ],
+    setup_requires=[
+    ],
+    cffi_modules=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'kmeans = kmeans:main'
+        ]
+    }
+)
