15379b488b71e07eee7f4c111b5e50f013663cd1
Andrew Gilbert <andrewg800@gmail.com>
2015-12-04 13:57:32 -0800

    Basic shape of find_frequent_itemsets

--- lab6/apriori.py
+++ lab6/apriori.py
@@ -8,6 +8,37 @@ def find_frequent_itemsets(data, minSup):
     # subsets of any other itemset) are now considered skyline
     # frequent itemsets (because there's no superset of them which can
     # appear)
+    F = []
+    F_k = [set(row) for i, row in data.iterrows() if support(i) >= minSup]
+    k = 1
+    while len(F_k) > 0:
+        candidates = candidate_generate(F_k, k)
+        count = []
+        for i, row in data.iterrows():
+            for j, candidate in enumerate(candidates):
+                if subseteq(candidate, row):
+                    count[j] += 1
+        F_k_next = []
+        tossed = []
+        for i, candidate in enumerate(candidates):
+            if count[i]/n >= minSup:
+                F_k_next.append(candidate)
+            else:
+                tossed.append(candidate)
+        tossed_subsets = set()
+        for failure in tossed:
+            tossed_subsets.add(subsets(failure, F_k))
+        for fnext in F_k_next:
+            tossed_subsets = [passed for passed in tossed_subsets if not subset(passed, fnext)]
+        F.append(*tossed_subsets)
+        k += 1
+
+def subset(sub, sup):
+    return len(symdiff(sub, sup)) == 0
+
+def subsets(sup, sub):
+    subs = [symdiff(sup, set(i)) for i in sup]
+    return intersect(subs, sub)
 
 def find_association_rules(freq_sets, minConf):
     """
