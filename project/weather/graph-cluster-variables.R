15d445fd681894da2fd42d2721cd1a346cb7eff0
Andrew Gilbert <andrewg800@gmail.com>
2015-12-07 15:18:54 -0800

    Use new idvars column

--- project/weather/graph-cluster-variables.R
+++ project/weather/graph-cluster-variables.R
@@ -2,9 +2,9 @@ library('ggplot2')
 
 source('read-dataset.R')
 
-keeps <- c(value_cols, "cluster")
+keeps <- c(value_cols, "cluster", idvars)
 data <- shaped[,(names(shaped) %in% keeps)]
-data <- reshape(data, direction="long", varying=X, timevar="ELEMENT", idvar="X")
+data <- reshape(data, direction="long", varying=value_cols, timevar="ELEMENT", idvar=idvars)
 
 graph <- ggplot(data, aes(cluster, VALUE)) +
   geom_boxplot(aes(fill=factor(cluster))) +
