ff97abdcfd2cc227346e1ad247ea82c4d6ddd1ea
Andrew Gilbert <andrewg800@gmail.com>
2015-12-07 15:18:40 -0800

    Add variable for id columns

--- project/weather/read-dataset.R
+++ project/weather/read-dataset.R
@@ -2,12 +2,14 @@ if (!exists('raw_dataset')) {
   raw_dataset <- read.csv('../data/weather/USW00093209.dly.csv')
   raw_dataset <- raw_dataset[raw_dataset$YEAR >= 2010,]
 
+  idvars = c("ID", "YEAR", "MONTH", "DAY")
   shaped <- reshape(
       raw_dataset[,c("ID","YEAR", "MONTH", "DAY", "ELEMENT", "VALUE")],
-      idvar=c("ID", "YEAR", "MONTH", "DAY"),
+      idvar=idvars,
       timevar=c("ELEMENT"),
       direction="wide")
   shaped$date <- ISOdate(shaped$YEAR, shaped$MONTH, shaped$DAY)
+  idvars <- c(idvars, "date")
 
   if (file.exists('../clusters.csv')) {
     raw_clusters <- read.csv('../clusters.csv')
