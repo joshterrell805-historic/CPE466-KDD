25f485d22e4a38e117349be4201977b18102a58a
Josh Terrell <joshterrell805@gmail.com>
2015-12-07 10:35:28 -0800

    use DBSCAN, write clusters to csv

--- project/scripts/cluster.py
+++ project/scripts/cluster.py
@@ -11,25 +11,23 @@ import click
 @click.argument('datafile', type=click.File('r'))
 @click.argument('k', type=click.INT)
 def main(datafile, k):
-    header = datafile.readline()
-    collist = [i for i, toggle in enumerate(header.split(',')) if toggle != "0"]
-    datafile.seek(0)
-    data = pd.read_csv(datafile, usecols=collist).as_matrix()
+    #usecols=None
+    #data = pd.read_csv(datafile, usecols=usecols)
+    #.as_matrix()
 
-    def metric(
-
-    pipeline = Pipeline([('dbscan', DBSCAN())])
+    #data.as_matrix()
+    data = np.random.rand(30,4)
+    pipeline = Pipeline([('clst', DBSCAN())])
     pipeline.set_params(**{
-        'dbscan__k': k,
-        'dbscan__delta_sse': 4,
+        'clst__eps': 0.5,
+        'clst__min_samples': 5,
     })
     pipeline.fit(data)
-    clf = pipeline.get_params()['clf']
-    print('\n'.join(str(c) for c in clf.clusters_))
-    dump_graph(data, clf.assignments_)
+    clst = pipeline.get_params()['clst']
+    dump_graph(data, clst.labels_)
 
-def dump_graph(X, assignments_):
+def dump_graph(X, labels_):
     X = pd.DataFrame(X)
-    X['assignment'] = assignments_
-    X.to_csv('graph.csv')
+    X['cluster'] = labels_
+    X.to_csv('clusters.csv')
 
