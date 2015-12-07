284065745451c2f6ae48c54af3bef28bf371aab6
Josh Terrell <joshterrell805@gmail.com>
2015-12-07 10:58:55 -0800

    cluster: write output with headers

--- project/scripts/cluster.py
+++ project/scripts/cluster.py
@@ -16,19 +16,18 @@ def main(datafile, k):
     #data = pd.read_csv(datafile, usecols=usecols)
     #.as_matrix()
 
-    #data.as_matrix()
-    data = np.random.rand(30,4)
+    #matrix = data.as_matrix()
+    matrix = np.random.rand(30,4)
     pipeline = Pipeline([('scaler', RobustScaler()), ('clusterer', DBSCAN())])
     pipeline.set_params(**{
-        'clusterer__eps': 0.5,
+        'clusterer__eps': 1.0,
         'clusterer__min_samples': 5,
     })
-    pipeline.fit(data)
+    pipeline.fit(matrix)
     clusterer = pipeline.get_params()['clusterer']
     dump_graph(data, clusterer.labels_)
 
 def dump_graph(X, labels_):
-    X = pd.DataFrame(X)
     X['cluster'] = labels_
     X.to_csv('clusters.csv')
 
