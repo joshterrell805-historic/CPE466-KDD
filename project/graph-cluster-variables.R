cff2cab0d3a3a442d3a35ebfcfcb52a5557441bd
Josh Terrell <joshterrell805@gmail.com>
2015-12-07 14:28:29 -0800

    read dataset in shared file

--- project/graph-cluster-variables.R
+++ project/graph-cluster-variables.R
@@ -1,14 +1,6 @@
 library('ggplot2')
 
-d <- read.csv('data/weather/USW00093209.dly.csv')
-d <- d[d$YEAR >= 2010,]
-shaped <- reshape(d[,c("ID","YEAR", "MONTH", "DAY", "ELEMENT", "VALUE")],
-                  idvar=c("ID", "YEAR", "MONTH", "DAY"),
-                  timevar=c("ELEMENT"),
-                  direction="wide")
-shaped$date <- ISOdate(shaped$YEAR, shaped$MONTH, shaped$DAY)
-c <- read.csv('clusters.csv')
-shaped$cluster <- factor(c$cluster)
+source('read-dataset.R')
 
 X <- c("VALUE.TMIN", "VALUE.TMAX", "VALUE.PRCP", "VALUE.AWND", "VALUE.WDF2",
            "VALUE.WDF5", "VALUE.WSF2", "VALUE.WSF5")
--- project/graph.R
+++ project/graph.R
@@ -1,16 +1,6 @@
 library('ggplot2')
 
-d <- read.csv('data/weather/USW00093209.dly.csv')
-d <- d[d$YEAR >= 2010,]
-
-shaped <- reshape(d[,c("ID","YEAR", "MONTH", "DAY", "ELEMENT", "VALUE")],
-                  idvar=c("ID", "YEAR", "MONTH", "DAY"),
-                  timevar=c("ELEMENT"),
-                  direction="wide")
-shaped$date <- ISOdate(shaped$YEAR, shaped$MONTH, shaped$DAY)
-
-c <- read.csv('clusters.csv')
-shaped$cluster <- factor(c$cluster)
+source('read-dataset.R')
 
 shaped <- shaped[shaped$YEAR == 2013 & shaped$MONTH > 10,]
 
