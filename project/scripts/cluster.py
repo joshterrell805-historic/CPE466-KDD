1d8959cccbc05c65125a08ddcd3b54fe8c3363f1
Josh Terrell <joshterrell805@gmail.com>
2015-12-07 11:19:44 -0800

    add arguments for eps and min points per cluster

--- project/scripts/cluster.py
+++ project/scripts/cluster.py
@@ -10,15 +10,16 @@ import click
 
 @click.command()
 @click.argument('datafile', type=click.File('r'))
-@click.argument('k', type=click.INT)
-def main(datafile, k):
+@click.argument('eps', type=click.FLOAT, default=1.0)
+@click.argument('min_samples', type=click.INT, default=5)
+def main(datafile, eps, min_samples):
     data = pd.read_csv(datafile, sep=' ')
 
     matrix = data.as_matrix()
     pipeline = Pipeline([('scaler', RobustScaler()), ('clusterer', DBSCAN())])
     pipeline.set_params(**{
-        'clusterer__eps': 1.0,
-        'clusterer__min_samples': 5,
+        'clusterer__eps': eps,
+        'clusterer__min_samples': min_samples,
     })
     pipeline.fit(matrix)
     clusterer = pipeline.get_params()['clusterer']
